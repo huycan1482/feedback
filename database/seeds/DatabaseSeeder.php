abb294862e57279504ba95b1cdbb1f1b0e36ca9d
huycan1482 <53845397+huycan1482@users.noreply.github.com>
2021-05-14 15:13:14 +0700

    create databaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,25 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('roles')->insert([
+            ['name' => 'admin'],
+            ['name' => 'manager'],
+            ['name' => 'teacher'],
+            ['name' => 'user']
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123456789'),
+            'role_id' => 1,
+            'gender' => 1,
+            'address' => 'Địa chỉ 1',
+            'date_of_birth' => '2021-05-13 00:00:00',
+            'code' => '12345678910',
+            'phone' => 0928583902,
+            
+        ]);
+
     }
 }
